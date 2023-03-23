2e600197cd8a296b0f4858fd11479f462b7c4547
Nathom78 <99360304+Nathom78@users.noreply.github.com>
2023-03-23 13:18:28 +0100

    Change auo-migration for make first migrate while install new base, or import from git the  project and migrate Else error while the first migrate, because the base with admin group and permissions doesn't exist before to add all permissions personalised

--- LITReview/authentication/admin.py
+++ LITReview/authentication/admin.py
@@ -13,12 +13,12 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('username', 'role', 'email', 'is_admin', 'is_active')
+    list_display = ('username', 'role', 'email', 'is_admin', 'is_active', 'is_superuser')
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('username', 'password', 'role')}),
         ('Personal info', {'fields': ('email',)}),
-        ('Permissions', {'fields': ('is_admin', 'is_active', 'groups')}),
+        ('Permissions', {'fields': ('is_admin', 'is_active', 'is_superuser', 'groups')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
